c2487effbdce1cc08505d1944fa0a06ffbdc73c2
xjt <969211735@qq.com>
2025-03-11 14:46:45 +0800

    stash, could work

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,40 @@
 #include "utils/parser.h"
 
 struct modeset_dev *panel;
-uint32_t image_data[1080 * 1920];
+uint32_t image_data[640 * 512];
+int panel_width{0}, panel_height{0}, offset_x{0}, offset_y{0};
 
-void panel_func(uint32_t width, uint32_t height)
+void panel_func()
 {
-    int fd = xDRM_Init(&panel, CONN_ID_DSI1, CRTC_ID_DSI1, PLANE_ID_DSI1, width, height, 0, 0);
+    int fd = xDRM_Init(&panel, CONN_ID_DSI1, CRTC_ID_DSI1, PLANE_ID_DSI1, 640, 512, 0, 0);
     xDRM_Draw(fd, panel);
     xDRM_Exit(fd, panel);
 }
 
 int main()
 {
-    auto config = Parser::readConfig("/oem/bootanimation/desc.ini");
+    // auto config = Parser::readConfig("/oem/bootanimation/desc.ini");
+    // panel_width = config->panel.width;
+    // panel_height = config->panel.height;
+    // offset_x = config->panel.offset_x;
+    // offset_y = config->panel.offset_y;
 
-    if (config)
+    std::thread th_panel = std::thread(panel_func);
+
+    // wait to finish initialize
+    sleep(1);
+    // usleep(300 * 1000);
+
+    int count = 0;
+    while (1)
     {
-        std::thread th_panel = std::thread(panel_func, config->panel.width, config->panel.height);
-        // wait to finish initialize
-        usleep(100 * 1000);
-
-        int count = 0;
-        while (1)
-        {
-            xDRM_Pattern(image_data, 1080, 1920, count++);
-            xDRM_Push(panel, image_data, sizeof(image_data));
-            usleep(16666);
-        }
-
-        if (th_panel.joinable())
-            th_panel.join();
+        xDRM_Pattern(image_data, 640, 512, count++);
+        xDRM_Push(panel, image_data, sizeof(image_data));
+        usleep(16 * 1000);
     }
+
+    if (th_panel.joinable())
+        th_panel.join();
 
     return 0;
 }
